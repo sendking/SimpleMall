82e10bb61a20a9ae996b1b80a5294c38b3fc412a
sk <shaokai.li@5th.work>
2018-12-28 22:34:54 +0800

    修复解码失败

--- config/express.js
+++ config/express.js
@@ -11,17 +11,18 @@ app.use(
     extended: true
   })
 );
-
 app.use("/api", router);
 app.use(morgan(process.env.NODE_ENV));
 app.use(
   session({
-    secret: "express_session",
+    secret: "express_cookie",
     resave: true,
     saveUninitialized: true,
     cookie: {
-      maxAge: 60 * 1000 * 60 //TODO:
-    }
+      maxAge: 60 * 1000 * 60
+    } //过期时间
+    // 这里设置的过期时间与token有效时间保持一致
   })
 );
+
 module.exports = app;
